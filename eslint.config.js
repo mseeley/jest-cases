cce33aa80066ff452b99dfa9aa0e0372f8f0c4d9
Matt Seeley <mattseeley@hotmail.com>
2019-02-14 00:05:05 -1000

    chore: correct eslint configuration
    ecmaVersion: 2018 allows for object spread

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
 
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     impliedStrict: true,
     sourceType: "script"
   },
